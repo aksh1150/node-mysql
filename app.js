ea0899cfa76d0df4f658fe1de5acde24e93a54c1
Akshit <akshit@Akshits-iMac.local>
2020-09-27 17:45:24 -0400

    Create index and registration form and log input values in the console

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const path = require("path");
 const mysql = require("mysql");
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env" });
 
 const db = mysql.createConnection({
-  host: "localhost", // IP address of the server
-  user: "root",
-  password: "",
-  database: "node-sql",
-  port: "8888",
+  host: process.env.HOST, // IP address of the server
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
 });
+
+const publicDirectory = path.join(__dirname, "./public");
+app.use(express.static(publicDirectory)); // to use all sttics file like css and JS by server use this line
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.set("view engine", "hbs");
+
 db.connect((err) => {
   if (err) {
     console.log(err);
@@ -17,9 +28,9 @@ db.connect((err) => {
   }
 });
 
-app.get("/", (req, res) => {
-  res.send("<h1>Server running</h1>");
-});
+// Define Routes
+app.use("/", require("./routes/pages"));
+app.use("/auth", require("./routes/auth"));
 
 app.listen(5000, () => {
   console.log("Server started on port 5000");
--- connection.js
+++
@@ -1,8 +0,0 @@
-const db = mysql.createConnection({
-  host: "localhost", // IP address of the server
-  user: "root",
-  password: "",
-  database: "node-sql",
-  port: "8080",
-});
-export default db;
